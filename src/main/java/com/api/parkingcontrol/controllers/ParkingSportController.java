d91dd6dd2f91534b18e72a1d9fc3fb465e9fcb51
Pablo <pabloh.anselmo@gmail.com>
2022-05-04 16:16:52 -0300

    Inserindo Paginação na requisição GetAll

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSportController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSportController.java
@@ -5,6 +5,10 @@ import com.api.parkingcontrol.dtos.ParkingSportDto;
 import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.services.ParkingSpotService;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -46,8 +50,9 @@ public class ParkingSportController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ParkingSpotModel>> getAllParkingSpots() {
-        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
+    public ResponseEntity<Page<ParkingSpotModel>> getAllParkingSpots(
+            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll(pageable));
     }
 
     @GetMapping(path = "/{id}")
